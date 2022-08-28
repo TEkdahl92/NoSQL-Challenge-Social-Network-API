46b40882c37cb12a28eb4e0c2b84069ffbfb6a38
TEkdahl92 <106709941+TEkdahl92@users.noreply.github.com>
2022-08-28 14:03:43 -0500

    required thought and user js's for index.js in models folder. also completed thought and user js

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model, Types } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+// create virtual that retrieves the length of the user's friends array field on query
+UserSchema.virtual('friendCount').get(function() {
+
+    return this.friends.length;
+})
+
+// create the User model using the UserSchema
+const User = model('User', UserSchema);
+
+// export the User model
+module.exports = User;
